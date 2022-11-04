7a9739ad316a1843752d0a0e646ddfddf3c64f77
Jo Wonyoung <ckstn0225@Gmail.com>
2022-11-04 18:33:00 +0900

    make Project

---
+++ src/main/java/Solutions.java
@@ -0,0 +1,8 @@
+public class Solutions {
+
+    public static void sout(){
+        System.out.println("hello World");
+    }
+
+
+}
---
+++ src/test/java/SolutionsTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class SolutionsTest {
+
+    private Solutions solutions;
+
+    @Test
+    void Sout() {
+        Solutions.sout();
+    }
+    @Test
+    void List() {
+        Solutions.sout();
+    }
+}
